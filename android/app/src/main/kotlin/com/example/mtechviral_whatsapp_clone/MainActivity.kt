c58b64f3e76984307706c250f9aaa2a2dc4f157d
nakamurusun <3872mn@gmail.com>
2020-11-29 01:37:58 +0900

    project is finished

---
+++ android/app/src/main/kotlin/com/example/mtechviral_whatsapp_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mtechviral_whatsapp_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/chat_model.dart
@@ -0,0 +1,59 @@
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({ this.name, this.message, this.time, this.avatarUrl });
+}
+
+List<ChatModel> dummyData = [
+  ChatModel(
+    name: 'Nakamura',
+    message: "Flutter is amazing, isn't it?!",
+    time: '15:30',
+    avatarUrl: "https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80"
+  ),
+  ChatModel(
+    name: 'Nakamur',
+    message: "Flutter is amazing, isn't it",
+    time: '15:30',
+    avatarUrl: "https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80"
+  ),
+  ChatModel(
+    name: 'Nakamu',
+    message: "Flutter is amazing",
+    time: '15:30',
+    avatarUrl: "https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80"
+  ),
+  ChatModel(
+    name: 'Nakam',
+    message: "Flutter is...",
+    time: '15:30',
+    avatarUrl: "https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80"
+  ),
+  ChatModel(
+    name: 'Naka',
+    message: "Flutter",
+    time: '15:30',
+    avatarUrl: "https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80"
+  ),
+  ChatModel(
+    name: 'Nak',
+    message: "Flutter is awesome",
+    time: '15:30',
+    avatarUrl: "https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80"
+  ),
+  ChatModel(
+    name: 'Na',
+    message: "Flutter is amazing, isn't it?!",
+    time: '15:30',
+    avatarUrl: "https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80"
+  ),
+  ChatModel(
+    name: 'N',
+    message: "Flutter is amazing, isn't it?!",
+    time: '15:30',
+    avatarUrl: "https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80"
+  ),
+];
